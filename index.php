31abd10feee5521a7f9b51390718b45fa8b2b891
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-06-21 15:34:40 +0200

    fix

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-    $paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  $paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-    $badWord = $_GET['badWord'];
+  $badWord = $_GET['badWord'];
     
-    $paragraphWithCensorship = str_replace($badWord, '***', $paragraph);
+  $paragraphWithCensorship = str_replace($badWord, '***', $paragraph);
 
 ?>
 
@@ -17,9 +17,9 @@
   <title>BadWord</title>
 </head>
 <body>
-<p>Paragrafo originale: <?= $paragraph; ?></p>
-<p>Lunghezza paragrafo originale: <?= strlen($paragraph); ?></p>
-<p>Paragrafo censurato: <?= $paragraphWithCensorship; ?></p>
-<p>Lunghezza paragrafo censurato: <?= strlen($paragraphWithCensorship); ?></p>
+  <p>Paragrafo originale: <?= $paragraph; ?></p>
+  <p>Lunghezza paragrafo originale: <?= strlen($paragraph); ?></p>
+  <p>Paragrafo censurato: <?= $paragraphWithCensorship; ?></p>
+  <p>Lunghezza paragrafo censurato: <?= strlen($paragraphWithCensorship); ?></p>
 </body>
 </html>
